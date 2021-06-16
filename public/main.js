8fef975f29bab6395a1125878ddb01c42b1cc212
Abhishek <kabhishekmandal@gmail.com>
2021-06-16 18:47:51 +0530

    make it better

--- public/main.js
+++ public/main.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', (e) => {
     const data = { os: choice };
 
 
-    fetch('https://realtimepoll2021.herokuapp.com/', {
+    fetch('/', {
         method: 'post',
         body: JSON.stringify(data),
         headers: new Headers({
@@ -19,7 +19,7 @@ form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 });
-fetch("https://realtimepoll2021.herokuapp.com/")
+fetch("/")
     .then(res => res.json())
     .then(data => {
         let votes = data.votes;
